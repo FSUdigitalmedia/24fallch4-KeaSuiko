0bfbed6b2f2a6e6f0890ab54cd9dd107df5d6ac2
KEA KAMIYA <KSK23A@FSU.EDU>
2024-09-13 14:46:36 -0400

    added Kamiya

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 
 let WFont;
 let y = 0;
-
 let speed = 5;  
 let WILKERSON;
 
@@ -14,22 +13,28 @@ function setup() {
   createCanvas(500, 500);
 }
 
-function draw() {
+function draw(){
   background(141, 19, 19)
   fill(255, 255, 255)
 
-  //let d= map(mouseY, 0, width, 0, 1)
+  //textFont(WFont)
+  textSize(43)
+  fill(0, 0, 0, mouseY)
+  text("Kamiya", 30, 20, 50, 50)
+
   let yPos = map2(y, 0, height, 0, height, SINUSOIDAL, BOTH)
   let r = mouseX;
   let p = mouseY;
-  image(WILKERSON, 0, yPos, yPos, yPos)
 
- if (p > 1500) { p = 1500}
-  if (r > 500) { r = 500}
+  if (keyIsPressed){
+  image(WILKERSON, 0, yPos, yPos, yPos)}
+
+  //if (p > 1500) { p = 1500}
+  //if (r > 500) { r = 500}
   
   if (y > height - 190 || y < 0){
     speed = speed * -1;
   }
   y = y + speed;
+} 
 
-}
