3bfa978f2f225680c9ef21e5a2c8f0ffacbb7327
KEA KAMIYA <KSK23A@FSU.EDU>
2024-09-13 14:26:20 -0400

    wilkerson

--- sketch.js
+++ sketch.js
@@ -1,27 +1,33 @@
 
 let WFont;
 let y = 0;
-let speed = 3;  
+
+let speed = 5;  
+let WILKERSON;
 
 function preload() {
   WFont = loadFont("assets/PlaywriteCU-VariableFont_wght.ttf")
+  WILKERSON = loadImage("assets/wilk.jpg")
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
-
 
 function draw() {
   background(141, 19, 19)
   fill(255, 255, 255)
-  textFont(WFont)
-  let d= map(mouseY, 0, width, 0, 1)
-  textSize(y)
-  let yPos = map2(y, 0, width, 0, width, SINUSOIDAL, BOTH)
-  text("WILKERSON", 70, yPos)
 
-  if (y > height || y < 0){
+  //let d= map(mouseY, 0, width, 0, 1)
+  let yPos = map2(y, 0, height, 0, height, SINUSOIDAL, BOTH)
+  let r = mouseX;
+  let p = mouseY;
+  image(WILKERSON, 0, yPos, yPos, yPos)
+
+ if (p > 1500) { p = 1500}
+  if (r > 500) { r = 500}
+  
+  if (y > height - 190 || y < 0){
     speed = speed * -1;
   }
   y = y + speed;
